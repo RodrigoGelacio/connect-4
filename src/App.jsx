eb08bad5a4e5ddc27ef9d565cf2cd43dbcddac62
yeicobF <jacobfloresrdz@gmail.com>
2025-03-31 22:42:28 -0600

    style: format `App` and change references to `modules/board`

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,40 @@
-import { isWinner } from "./logic/board.js";
-import { useState } from "react";
-import { Column } from "./components/Column.jsx";
-import { numRows, numCols } from "./constants.js";
-import "./App.css";
-import { WinnerModal } from "./components/winnerModal.jsx";
+import { isWinner } from "@/modules/board/lib/logic"
+import { useState } from "react"
+import { Column } from "./components/Column.jsx"
+import { numRows, numCols } from "@/modules/board/constants"
+import "./App.css"
+import { WinnerModal } from "./components/winnerModal.jsx"
 
 function App() {
-  const [winner, setWinner] = useState(false);
-  const [restart, setRestart] = useState(false);
-  const [turn, setTurn] = useState(true);
+  const [winner, setWinner] = useState(false)
+  const [restart, setRestart] = useState(false)
+  const [turn, setTurn] = useState(true)
   const [board, setBoard] = useState(
     Array.from({ length: numRows }, () => Array(numCols).fill(null)),
-  );
+  )
 
-  const colArray = Array.from({ length: numCols }, (_, index) => index);
+  const colArray = Array.from({ length: numCols }, (_, index) => index)
 
   const restartGame = () => {
-    setWinner(false);
-    setTurn(true);
-    setBoard(Array.from({ length: numRows }, () => Array(numCols).fill(null)));
-    setRestart(!restart);
+    setWinner(false)
+    setTurn(true)
+    setBoard(Array.from({ length: numRows }, () => Array(numCols).fill(null)))
+    setRestart(!restart)
     // document.querySelectorAll(".circle").forEach((elem) => {
     //   elem.className = "circle";
     // });
-  };
+  }
 
   const updateBoard = (row, col) => {
-    const newBoard = [...board];
-    newBoard[row][col] = turn;
+    const newBoard = [...board]
+    newBoard[row][col] = turn
 
     if (isWinner(newBoard)) {
-      setWinner(true);
+      setWinner(true)
     } else {
-      setTurn(!turn);
+      setTurn(!turn)
     }
-  };
+  }
 
   return (
     <div className="board-container">
@@ -50,7 +50,7 @@ function App() {
               rows={numRows}
               restart={restart}
             ></Column>
-          );
+          )
         })}
       </div>
       <div className="">{turn ? "Player 1" : "Player 2"} turn!</div>
@@ -60,7 +60,7 @@ function App() {
         turn={turn}
       ></WinnerModal>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
