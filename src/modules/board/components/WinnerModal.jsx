0c11907eb9ec485ca34b9375dd1604d6441d5916
yeicobF <jacobfloresrdz@gmail.com>
2025-04-03 01:02:13 -0600

    feat: create `RestartButton`

--- src/modules/board/components/WinnerModal.jsx
+++ src/modules/board/components/WinnerModal.jsx
@@ -1,18 +1,15 @@
 import { Circle } from "@/modules/board/components/Circle"
+import { RestartButton } from "./RestartButton"
 
 export function WinnerModal({ winner, turn, restartGame }) {
   if (!winner) return null
-
-  const handleClick = () => {
-    restartGame()
-  }
 
   return (
     <div className="winner-modal">
       <div className="winner-container">
         <Circle turn={turn} />
         <h1 className="winner-title">wins!</h1>
-        <button onClick={handleClick}>Restart game</button>
+        <RestartButton restartGame={restartGame} />
       </div>
     </div>
   )
